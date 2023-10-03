edd8c1e5fd83aa53b61665711f9e67790a91ebdc
bathio-git <ecrire@bathio.xyz>
2023-10-03 14:04:04 -0400

    1.016

--- src/Context/Radios.js
+++ src/Context/Radios.js
@@ -2,7 +2,8 @@ class Radio {
     constructor( name, slogans, streams, radioUrl, id ) {
         this.name = name;
         this.slogan = slogans;
-        this.stream = `${process.env.NEXT_PUBLIC_API_URL}/api/8000?url=${streams}`;
+        /* this.stream = `${process.env.NEXT_PUBLIC_API_URL}/api/8000?url=${streams}`; */
+        this.stream = streams;
         this.radioUrl = radioUrl;
         this.id = id;
     }
